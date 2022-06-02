2e5cd87f2c3ed48cd42cf9a04e3bf967929ae8f6
panjianpeng <1456298364@qq.com>
2022-06-02 09:48:50 +0800

    第二次

--- src/main/java/com/atguigu/gitText.java
+++ src/main/java/com/atguigu/gitText.java
@@ -4,5 +4,6 @@ public class gitText {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("第一次");
+        System.out.println("第二次");
     }
 }
