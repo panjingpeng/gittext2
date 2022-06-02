c03abe0c71f83ca001115a18d2bdf027ceff50fc
dw <1987907279@qq.com>
2022-06-02 13:50:29 +0800

    添加新文件

---
+++ src/main/java/com/atguigu/NewFile.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * Date:2022/6/2
+ * Author:dw
+ * Description:
+ */
+public class NewFile {
+
+    public static void main(String[] args) {
+        System.out.println("添加了一个新的文件");
+    }
+}
